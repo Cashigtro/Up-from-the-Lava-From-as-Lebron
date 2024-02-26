f9c9c784c6d3bf2e9958d20c33f356bcb8dfabd4
cash-i1 <cashignitro@gmail.com>
2024-02-26 19:03:32 +1100

    tried to get firebase leaderboard to work (it doesn't)

--- scripts/leaderboard.gd
+++ scripts/leaderboard.gd
@@ -1,7 +1,7 @@
 extends HTTPRequest
 
 @onready var people = $"../people"
-var url = "http://dreamlo.com/lb/cDBT1v2pkEC_cuoZy7-BzQEO5o81ms5kanfKzY2LKYdQ"
+var url = "https://up-from-the-lava-from-as-lebro-default-rtdb.asia-southeast1.firebasedatabase.app/"
 var leaderboard
 var method = HTTPClient.METHOD_GET
 var arrow = preload("res://assets/images/arrow_right.png")
@@ -13,7 +13,7 @@ signal more_info_button_pressed(rank, player_name)
 
 func _ready():
     connect("request_completed", populate_leaderboard)
-    request(url + "/json-seconds", PackedStringArray(), method)
+    request(url + "/scores.json", PackedStringArray(), method)
     
 func clear_scores():
     for i in people.get_children():
@@ -49,6 +49,60 @@ func populate_leaderboard(result, response_code, header, body):
     if body:
         var leaderboard = JSON.parse_string(body.get_string_from_utf8())
         print(leaderboard)
+        if leaderboard != null:
+            print(len(leaderboard))
+            if len(leaderboard) != 1:
+                print(leaderboard)
+                var leaderboard_objects = leaderboard
+                print(leaderboard_objects)
+                
+                clear_scores()
+                
+                var count = 0
+                for i in leaderboard_objects:
+                    var label = Label.new()
+                    print(i)
+                    label.text = (str(count+1) + "# " + i + ": " + str(leaderboard[i]["data"]["score"]))
+                    if count == 0:
+                        label.label_settings = LabelSettings.new()
+                        
+                        label.label_settings.font_size = 25
+                        label.label_settings.font_color = Color(0,255,0)
+                    elif count == 1:
+                        label.label_settings = LabelSettings.new()
+                        label.label_settings.font_size = 20
+                        label.label_settings.font_color = Color(150,255,0)
+                    elif count == 2:
+                        label.label_settings = LabelSettings.new()
+                        label.label_settings.font_size = 15
+                        label.label_settings.font_color = Color(Color.CHOCOLATE)
+                    else:
+                        label.label_settings = LabelSettings.new()
+                        label.label_settings.font_size = 12
+                        label.label_settings.font_color = Color(255,255,255)
+                        
+                    label.text = (str(count+1) + "# " + i + ": " + get_time(int(leaderboard[i]["data"]['score'])))
+
+
+                    people.add_child(label)
+                    count += 1
+            
+            else:
+                error.text = "there is only one person on the\nleaderboard which mean my\ncode breaks for some reason\nand i cant be bothered to fix it"
+                    
+            
+        else:
+            error.text = "there are no scores to show"
+    
+    else:
+        people.get_child(0).text = "there is no one on the leader board!"
+
+
+func populate_leaderboard_old(result, response_code, header, body):
+    
+    if body:
+        var leaderboard = JSON.parse_string(body.get_string_from_utf8())
+        print(leaderboard)
         if leaderboard["dreamlo"]["leaderboard"] != null:
             print(len(leaderboard["dreamlo"]["leaderboard"]["entry"]))
             if len(leaderboard["dreamlo"]["leaderboard"]["entry"]) != 1:
@@ -62,7 +116,6 @@ func populate_leaderboard(result, response_code, header, body):
                 for i in leaderboard_objects:
                     var label = Label.new()
                     label.text = (str(count+1) + "# " + i['name'] + ": " + i['score'])
-                    
                     if count == 0:
                         label.label_settings = LabelSettings.new()
                         
@@ -97,4 +150,3 @@ func populate_leaderboard(result, response_code, header, body):
     else:
         people.get_child(0).text = "there is no one on the leader board!"
 
-        
